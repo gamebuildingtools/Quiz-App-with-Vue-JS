a2268795ce2734448f7858c46f7d21145676d7eb
Daniel Williams <dwilliams@talktotucker.com>
2017-12-07 13:58:55 -0500

    Displaying data with string interpolation

--- quizzer/src/App.vue
+++ quizzer/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="ui centered card">
+      <div class="content">
+        <div class="header">
+          {{title}}
+        </div>
+        <div class="meta">
+          {{description}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      title: "Chapter 1",
+      description: "This is a sample description."
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
