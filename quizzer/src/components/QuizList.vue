a23e1474b2b0f3343755255cd092b78c201b8978
Daniel Williams <dwilliams@talktotucker.com>
2017-12-21 09:38:58 -0500

    Create a Flash-Card Component

--- quizzer/src/components/QuizList.vue
+++ quizzer/src/components/QuizList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <quiz v-for="quiz in quizzes" :key="quiz.id" v-bind:quiz="quiz"></quiz>
+
+    <div class="ui cards">
+      <flash-card v-for="card in quizzes[0].cards" :key="card.id" v-bind:card="card"></flash-card>
+    </div>
+    
   </div>
 </template>
 
@@ -8,11 +13,13 @@
 <script>
 
 import Quiz from './Quiz';
+import FlashCard from './FlashCard';
 
 export default {
   props: ['quizzes'],
   components: {
-    Quiz
+    Quiz,
+    FlashCard,
   }
 }
 </script>
